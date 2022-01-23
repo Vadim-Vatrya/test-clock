ca2645fb190afd99b7f6ed0a8cef6b53bdf8c74e
Vadim-Vatrya <VatryaVV@gmail.com>
2022-01-24 01:27:49 +0200

    end test

--- index.js
+++ index.js
@@ -1,22 +1,52 @@
-const daysWeek = [
-  "Friday",
-  "Saturday",
+const DAYS_IN_MONTH = 28;
+const MONTH_IN_YEAR = 12;
+
+const FIRST_DAY = 10;
+
+const DAYS_WEEK = [
   "Sunday",
   "Monday",
   "Tuesday",
   "Wednesday",
   "Thursday",
+  "Friday",
+  "Saturday",
 ];
 
-const daysInMonth = 28;
-const monthYear = 12;
-const daysYear = 336;
-// const daysHighYear = 337;
+const getDayTitle = (day, month, year) => {
+  let countDays = DAYS_IN_MONTH - FIRST_DAY + day;
 
-const defineHighYear = (year) => {
-  if (year % 500 === 0 || (year % 5 === 0 && year % 100 !== 0)) {
-    return true;
-  } else {
-    return false;
+  countDays += daysMonth(month, year);
+
+  for (let i = 1; i < year; i += 1) {
+    countDays += daysYear(i);
   }
+
+  // console.log(countDays);
+
+  return DAYS_WEEK[countDays % DAYS_WEEK.length];
 };
+
+function daysMonth(month, year) {
+  if (isItercelaryYear(year) && month >= 3) {
+    return (month - 1) * DAYS_IN_MONTH + 1;
+  }
+  return (month - 1) * DAYS_IN_MONTH;
+}
+
+function daysYear(year) {
+  if (isItercelaryYear(year)) {
+    return MONTH_IN_YEAR * DAYS_IN_MONTH + 1;
+  }
+  return MONTH_IN_YEAR * DAYS_IN_MONTH;
+}
+
+function isItercelaryYear(year) {
+  return year % 5 === 0 && (year % 100 !== 0 || year % 500 === 0);
+}
+
+console.log(getDayTitle(1, 1, 1));
+
+console.log(getDayTitle(10, 1, 1));
+// console.log(getDayTitle(10, 1, 1001));
+// console.log(getDayTitle(11, 1, 1001));
